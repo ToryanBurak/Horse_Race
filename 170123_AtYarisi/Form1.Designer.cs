b9bff443c775b657a2744ddabd37de11ea00da89
ToryanBurak <buraktoryan@gmail.com>
2023-01-18 01:55:28 +0300

    Program Files

---
+++ 170123_AtYarisi/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace _170123_AtYarisi
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.horse1 = new System.Windows.Forms.PictureBox();
+            this.horse2 = new System.Windows.Forms.PictureBox();
+            this.horse3 = new System.Windows.Forms.PictureBox();
+            this.horse4 = new System.Windows.Forms.PictureBox();
+            this.cmb_Horse_Select = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.race_Timer = new System.Windows.Forms.Timer(this.components);
+            this.Horse_Power_Timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.horse1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.horse2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.horse3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.horse4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(-10, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1801, 23);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(-10, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(1801, 23);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(-10, 273);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(1801, 23);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(-10, 380);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(1801, 23);
+            this.button4.TabIndex = 7;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(-10, 484);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(1801, 23);
+            this.button5.TabIndex = 8;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // horse1
+            // 
+            this.horse1.Enabled = false;
+            this.horse1.Image = global::_170123_AtYarisi.Properties.Resources.Horse_Gif;
+            this.horse1.Location = new System.Drawing.Point(12, 89);
+            this.horse1.Name = "horse1";
+            this.horse1.Size = new System.Drawing.Size(100, 67);
+            this.horse1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.horse1.TabIndex = 1;
+            this.horse1.TabStop = false;
+            // 
+            // horse2
+            // 
+            this.horse2.Enabled = false;
+            this.horse2.Image = global::_170123_AtYarisi.Properties.Resources.Horse_Gif;
+            this.horse2.Location = new System.Drawing.Point(12, 200);
+            this.horse2.Name = "horse2";
+            this.horse2.Size = new System.Drawing.Size(100, 67);
+            this.horse2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.horse2.TabIndex = 9;
+            this.horse2.TabStop = false;
+            // 
+            // horse3
+            // 
+            this.horse3.Enabled = false;
+            this.horse3.Image = global::_170123_AtYarisi.Properties.Resources.Horse_Gif;
+            this.horse3.Location = new System.Drawing.Point(12, 302);
+            this.horse3.Name = "horse3";
+            this.horse3.Size = new System.Drawing.Size(100, 67);
+            this.horse3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.horse3.TabIndex = 10;
+            this.horse3.TabStop = false;
+            // 
+            // horse4
+            // 
+            this.horse4.Enabled = false;
+            this.horse4.Image = global::_170123_AtYarisi.Properties.Resources.Horse_Gif;
+            this.horse4.Location = new System.Drawing.Point(12, 411);
+            this.horse4.Name = "horse4";
+            this.horse4.Size = new System.Drawing.Size(100, 67);
+            this.horse4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.horse4.TabIndex = 11;
+            this.horse4.TabStop = false;
+            // 
+            // cmb_Horse_Select
+            // 
+            this.cmb_Horse_Select.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_Horse_Select.FormattingEnabled = true;
+            this.cmb_Horse_Select.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.cmb_Horse_Select.Location = new System.Drawing.Point(99, 16);
+            this.cmb_Horse_Select.Name = "cmb_Horse_Select";
+            this.cmb_Horse_Select.Size = new System.Drawing.Size(141, 21);
+            this.cmb_Horse_Select.TabIndex = 12;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Corbel", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.Location = new System.Drawing.Point(288, 15);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 23);
+            this.btnStart.TabIndex = 13;
+            this.btnStart.Text = "Başla";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Corbel", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStop.Location = new System.Drawing.Point(413, 15);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(100, 23);
+            this.btnStop.TabIndex = 14;
+            this.btnStop.Text = "Yarışı Bitir";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Bahsi Gir";
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Enabled = false;
+            this.btnEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEnd.Location = new System.Drawing.Point(1718, 50);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(26, 467);
+            this.btnEnd.TabIndex = 16;
+            this.btnEnd.Text = "BİTİŞ";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // race_Timer
+            // 
+            this.race_Timer.Interval = 50;
+            this.race_Timer.Tick += new System.EventHandler(this.race_Timer_Tick);
+            // 
+            // Horse_Power_Timer
+            // 
+            this.Horse_Power_Timer.Interval = 3000;
+            this.Horse_Power_Timer.Tick += new System.EventHandler(this.Horse_Power_Timer_Tick);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1769, 501);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cmb_Horse_Select);
+            this.Controls.Add(this.horse4);
+            this.Controls.Add(this.horse3);
+            this.Controls.Add(this.horse2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.horse1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form";
+            this.Text = "Hipodrom";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.horse1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.horse2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.horse3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.horse4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox horse1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox horse2;
+        private System.Windows.Forms.PictureBox horse3;
+        private System.Windows.Forms.PictureBox horse4;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.ComboBox cmb_Horse_Select;
+        private System.Windows.Forms.Timer race_Timer;
+        private System.Windows.Forms.Timer Horse_Power_Timer;
+    }
+}
+
