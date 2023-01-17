7d948afbb1aa0f8bf0704b341a9748899c911f46
ToryanBurak <buraktoryan@gmail.com>
2023-01-18 01:51:54 +0300

    Enums

---
+++ 170123_AtYarisi/Enums/Horse Select.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _170123_AtYarisi
+{
+    public enum Horse_Select
+    {
+        NoSelect=0,Horse1=1,Horse2=2,Horse3=3,Horse4=4
+    }
+}
---
+++ 170123_AtYarisi/Enums/Winner_Horse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _170123_AtYarisi.Enums
+{
+    public enum Winner_Horse
+    {
+        Horse1=1, Horse2=2,Horse3=3,Horse4=4
+    }
+}
